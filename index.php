72864a13c3a6fd329421abd8d4a525f88dccfccd
adityaxx21 <adityayatma@gmail.com>
2020-09-09 21:54:58 +0700

    input

--- index.php
+++ index.php
@@ -87,6 +87,7 @@ switch (ENVIRONMENT)
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'The application environment is not set correctly.';
 		exit(1); // EXIT_ERROR
+		//call you min
 }
 
 /*
